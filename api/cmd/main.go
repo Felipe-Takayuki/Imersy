99553363b61ff10b44603c87076008be0dbea1bd
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-08-30 19:48:33 -0300

    added connection with database

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/Felipe-Takayuki/Imersy/api/internal/router"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	c := router.Router()
+	dbHost := os.Getenv("DB_HOST")
+	password := "root"
+	if dbHost == "" {
+		dbHost = "127.0.0.1"
+		password = ""
+	}
+	db, err := sql.Open("mysql", "root:"+password+"@tcp("+dbHost+":3306)/imersy")
+	if err != nil {
+		panic(err.Error())
+	}
+	r := router.Router(db)
 	fmt.Println("Server is running on port 3000")
-	http.ListenAndServe(":3000", c)
+	http.ListenAndServe(":3000", r)
 }
