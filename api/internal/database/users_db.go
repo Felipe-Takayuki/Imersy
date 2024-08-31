bab1f297a2a9cce86beab9c389bd568b941c8eef
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-08-31 17:20:05 -0300

    add new funcs: send project, get projects send evaluation and get rank

--- api/internal/database/users_db.go
+++ api/internal/database/users_db.go
@@ -41,4 +41,195 @@ func (udb * UserDB) LoginUser(email, password string) (*model.User, error) {
 	return &user, nil
 }
 	
+func (udb *UserDB) SendProject(title,description, videoUrl, projectUrl string, ownerID int) (*model.Project, error) {
+	project := model.NewProject(title, description, videoUrl, projectUrl )
+	sendProject, err := udb.db.Exec("INSERT INTO PROJECT(name, description, video_url, github_url, status) VALUES (?, ?, ?, ?, ?, ?)", project.Name, project.Description, project.VideoUrl, project.ProjectUrl, "enviado")
+	if err != nil {
+		return nil, err 
+	}
+	project.Status = "enviado"
+	project.ID, err = sendProject.LastInsertId()
+	if err != nil {
+		return nil, err 
+	}
+	_, err = udb.db.Exec("INSERT INTO PROJECT_OWNER(user_id, project_id) VALUES (?, ?)", ownerID, project.ID)
+	if err != nil {
+		return nil, err 
+	}
+	_, err = udb.db.Exec("INSERT INTO SUBMITTED_PROJECT(bootcamp_id, project_id) VALUES (?, ?)", 1, project.ID)
+	if err != nil {
+		return nil, err 
+	}
+	return project, nil
+}
+
+func (udb *UserDB) GetProjects() ([]*model.Project, error) {
+	query:= `
+	SELECT p.id, p.name, u.name, p.description, p.video_url, p.github_url FROM PROJECT p 
+	JOIN PROJECT_OWNER po ON po.project_id = p.id 
+	JOIN USER u on u.id = po.user_id`
+
+	rows, err := udb.db.Query(query) 
+	if err != nil {
+		return nil, err 
+	}
+	var projects []*model.Project
+	for rows.Next() {
+		var project model.Project 
+		err := rows.Scan(&project.ID, &project.Name, &project.OwnerName, &project.Description, &project.VideoUrl, &project.ProjectUrl)
+		if err != nil {
+			return nil, err 
+		}
+		projects = append(projects, &project)
+	}
+	return projects, nil 
+}
+
+func (udb *UserDB) GetProjectsByCategorie(categorie string)([]*model.Project, error) {
+	query:= `
+	SELECT p.id, p.name, u.name, p.description, p.video_url, p.github_url FROM PROJECT p 
+	JOIN PROJECT_OWNER po ON po.project_id = p.id 
+	JOIN USER u on u.id = po.user_id 
+	WHERE p.teach_type = ?`
+	rows, err := udb.db.Query(query, categorie) 
+	if err != nil {
+		return nil, err 
+	}
+	var projects []*model.Project
+	for rows.Next() {
+		var project model.Project 
+		err := rows.Scan(&project.ID, &project.Name, &project.OwnerName, &project.Description, &project.VideoUrl, &project.ProjectUrl)
+		if err != nil {
+			return nil, err 
+		}
+		projects = append(projects, &project)
+	}
+	return projects, nil 
+}
+
+func (udb *UserDB) SendEvaluationProject(evaluatorID, projectID, qualityGrade, creativityGrade int64)  error {
+	_, err := udb.db.Exec("DELETE FROM SUBMITTED_PROJECT where project_id = ?", projectID)
+	if err != nil {
+		return err 
+	}
+	_, err = udb.db.Exec("INSERT INTO EVALUATED_PROJECT(evaluator_id, project_id, quality_grade, creativity_grade) VALUES (?, ?, ?, ?)", evaluatorID, projectID, qualityGrade, creativityGrade) 
+	if err != nil {
+		return err 
+	}
+	_, err = udb.db.Exec("UPDATE PROJECT SET status = ? where id = ?", "avaliado",projectID)
+	if err != nil {
+		return err 
+	}
+	return nil 
+}
+// func (udb *UserDB) GetProjectByID(projectID int64) (*model.Project, error) {
+// 	project:= &model.Project{}
+
+// 	query:= `
+// 	SELECT p.id, p.name, u.name, p.description, p.video_url, p.github_url FROM PROJECT p 
+// 	JOIN PROJECT_OWNER po ON po.project_id = p.id 
+// 	JOIN USER u on u.id = po.user_id WHERE p.id= ?`
 	
+// 	err := udb.db.QueryRow(query, projectID).Scan(project.ID, project.Name, project.OwnerName,project.Description, project.VideoUrl, project.ProjectUrl)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return project, nil 
+
+
+// }
+func (udb *UserDB) GetProjectByUserID(id int64) (*model.Project, error) {
+	project:= &model.Project{}
+	query:= `
+	SELECT p.id, p.name, u.name, p.description, p.video_url, p.github_url FROM PROJECT p 
+	JOIN PROJECT_OWNER po ON po.project_id = p.id 
+	JOIN USER u on u.id = po.user_id WHERE u.id= ?`
+	err := udb.db.QueryRow(query, id).Scan(project.ID, project.Name, project.OwnerName,project.Description, project.VideoUrl, project.ProjectUrl)
+	if err != nil{
+		return nil, err
+	}
+	return project, nil 
+}
+
+func (udb *UserDB) GetTopRankProjectsByCategorie(categorie string) ([]*model.Project, error) {
+	query:= `
+	SELECT p.id, p.name, u.name, p.description, p.video_url, p.github_url, AVG((ep.quality_grade + ep.creativity_grade) / 2.0) AS average_grade
+	FROM PROJECT p
+	JOIN EVALUTED_PROJECT ep ON p.id = ep.project_id
+	JOIN PROJECT_OWNER po ON p.id = po.project_id
+	JOIN USER u ON u.id = po.user_id
+	GROUP BY p.id, p.name, u.name, p.description, p.video_url, p.github_url
+	ORDER BY average_grade DESC
+	LIMIT 3
+	WHERE u.teach_type = ?
+`
+	rows, err := udb.db.Query(query, categorie)
+	if err != nil {
+		return nil, err 
+	}
+	var topRanks []*model.Project
+	for rows.Next() {
+		var topRank model.Project
+		err := rows.Scan(&topRank.ID, &topRank.Name, &topRank.OwnerName, &topRank.Description, &topRank.VideoUrl, &topRank.ProjectUrl, &topRank.Average)
+		if err != nil {
+			return nil, err 
+		}
+		topRanks = append(topRanks, &topRank)
+	}
+	return topRanks, nil 
+
+}
+func (udb *UserDB) SendMaterialClass(title, subject, content string, userID int64) (*model.MaterialClass, error) {
+	materialClass := model.NewMaterialClass(title, subject, content)
+	sendMaterial, err := udb.db.Exec("INSERT INTO CLASS_MATERIAL(title, subject, content) VALUES (?,?,?)", materialClass.Title, materialClass.Subject, materialClass.Content)
+	if err != nil {
+		return nil, err 
+	}
+	materialClass.ID , err = sendMaterial.LastInsertId()
+	if err != nil {
+		return nil, err 
+	}
+	_, err = udb.db.Exec("INSERT INTO OWNER_MATERIAL(material_id, user_id) VALUES (?, ?)", materialClass.ID, userID)
+	if err != nil {
+		return nil, err 
+	}
+	return materialClass, nil 
+}
+
+
+func (udb *UserDB) GetMaterialsClass() ([]*model.MaterialClass, error) {
+	query:= `
+	SELECT m.id, m.title, u.name, m.subject, m.content FROM CLASS_MATERIAL m 
+	JOIN OWNER_MATERIAL om ON om.material_id = m.id
+	JOIN USER u ON u.id = om.user_id
+	`
+	rows, err := udb.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var materials []*model.MaterialClass
+	for rows.Next() {
+		var material model.MaterialClass 
+		if err := rows.Scan(&material.ID, &material.Title, &material.OwnerName, &material.Subject, &material.Content); err != nil {
+			return nil, err
+		}
+		materials = append(materials, &material)
+	}
+	return materials, nil
+}
+
+func (udb *UserDB) GetMaterialClassByID(materialID int64) (*model.MaterialClass, error) {
+	material := &model.MaterialClass{}
+	query:= `
+	SELECT m.id, m.title, u.name, m.subject, m.content FROM CLASS_MATERIAL m 
+	JOIN OWNER_MATERIAL om ON om.material_id = m.id
+	JOIN USER u ON u.id = om.user_id
+	WHERE m.id = ?
+	`
+	err := udb.db.QueryRow(query, materialID).Scan(material.ID, material.Title, material.OwnerName, material.Subject, material.Content)
+	if err != nil {
+		return nil, err 
+	}
+	return material, nil
+}	
