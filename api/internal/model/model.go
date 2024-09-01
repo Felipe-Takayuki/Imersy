606aacb46b3652ad724b5ca7e776c7c9f7acaa9d
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-08-31 21:35:47 -0300

    fix: update function "new user"

--- api/internal/model/model.go
+++ api/internal/model/model.go
@@ -9,8 +9,8 @@ type User struct {
 	Cpf         string `json:"cpf"`
 	Scholl      string `json:"scholl"`
 	PhoneNumber int    `json:"phone_number"`
-	HighScholl 	int 	`json:"high_scholl"`
-	TeachType   string 	`json:"teach_type"`
+	HighScholl  int    `json:"high_scholl"`
+	TeachType   string `json:"teach_type"`
 }
 
 func NewUser(userType, name, email, password, cpf, scholl, teachType string, highScholl, phoneNumber int) *User {
@@ -21,27 +21,58 @@ func NewUser(userType, name, email, password, cpf, scholl, teachType string, hig
 		Password:    password,
 		Cpf:         cpf,
 		Scholl:      scholl,
-		HighScholl: highScholl,
-		TeachType: teachType,
+		HighScholl:  highScholl,
+		TeachType:   teachType,
 		PhoneNumber: phoneNumber,
 	}
 }
 
 type Project struct {
-	ID          int64  `json:"id"`
-	Name        string `json:"title"`
-	OwnerName   string `json:"owner_name"`
-	Description string `json:"description"`
-	VideoUrl    string `json:"video_url"`
-	ProjectUrl  string `json:"project_url"`
+	ID              int64  `json:"id"`
+	Name            string `json:"title"`
+	OwnerName       string `json:"owner_name"`
+	Description     string `json:"description"`
+	VideoUrl        string `json:"video_url"`
+	ProjectUrl      string `json:"project_url"`
+	Status          string `json:"status"` // enviado || avaliado
+	Average 		float64 `json:"avarage"`
 }
 
+func NewProject(name, description, videoUrl, projectUrl string) *Project {
+	return &Project{
+		Name:        name,
+		Description: description,
+		VideoUrl:    videoUrl,
+		ProjectUrl:  projectUrl,
+	}
+}
+
+type Evaluation struct {
+	ProjectID int64 `json:"project_id"`
+	QualityGrade int64 `json:"quality_grade"`
+	CreativityGrade int64 `json:"creativity_grade"`
+}
 type Bootcamp struct {
-	ID          int64  `json:"id"`
-	Title       string `json:"title"`
-	Address     string `json:"address"`
+	ID          int64   `json:"id"`
+	Title       string  `json:"title"`
+	Address     string  `json:"address"`
 	Subscribers []*User `json:"subscribers"`
-	StartDate   string `json:"start_date"`
-	EndDate     string `json:"end_date"`
+	StartDate   string  `json:"start_date"`
+	EndDate     string  `json:"end_date"`
 }
 
+type MaterialClass struct {
+	ID        int64  `json:"id"`
+	Title     string `json:"title"`
+	OwnerName string `json:"owner_name"`
+	Subject   string `json:"subject"`
+	Content   string `json:"content"`
+}
+
+func NewMaterialClass(title, subject, content string) *MaterialClass {
+	return &MaterialClass{
+		Title:   title,
+		Subject: subject,
+		Content: content,
+	}
+}
