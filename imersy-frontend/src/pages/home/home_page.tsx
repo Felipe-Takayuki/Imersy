0747237c45e8d6fa0ca8e6cd3ccb1e0bac5ad543
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-09-01 10:58:01 -0300

    init frontend

---
+++ imersy-frontend/src/pages/home/home_page.tsx
@@ -0,0 +1,11 @@
+export function HomePage() {
+    return (
+        <>
+            <div> 
+              <p>Imersy</p>
+              <button>Inscreva-se</button>
+              <button>Entre</button>
+            </div>
+        </>
+    )
+}
