14bb9ecaecdddf45396649362e94258f6245bdcf
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-09-02 00:10:41 -0300

    insert react router dom in project

--- imersy-frontend/src/App.tsx
+++ imersy-frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { HomePage } from "./pages/home/home_page"
+import { RegisterPage } from "./pages/register/register_page"
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/:bootcamp_id/register",
+    element: <RegisterPage />
+  }
+])
 export function App() {
-  return (
-    <h1 className="text-3xl font-sans text-black underline">
-      Hello world!
-    </h1>
-  )
+  return <RouterProvider router={router} />
 }
