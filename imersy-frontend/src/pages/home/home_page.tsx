b4c550534a88091d846129d303b723960342a06a
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-09-06 14:09:57 -0300

    responsividade

--- imersy-frontend/src/pages/home/home_page.tsx
+++ imersy-frontend/src/pages/home/home_page.tsx
@@ -10,33 +10,33 @@ export function HomePage() {
         <>
             <NavBar />
             <main className="h-full">
-              <section className=" flex w-3/4 p-10 flex-col justify-center-center">
-                <p className="font-light text-6xl text-white ">
+              <section className=" flex w-3/4 p-10 flex-col max-md:p-2  justify-center-center">
+                <p className="font-light text-6xl max-md:text-5xl text-white ">
                     Aproveite esta oportunidade<br />
                     E participe do nosso
                     <b className="font-bold"> Bootcamp <br /> Jovem Programador!</b>
                 </p> 
-                <button onClick={() => navigate("/register")} className="w-96 mt-10 h-36 text-5xl font-medium text-white text-center rounded-2xl bg-blue-2 mr-4 mb-40">Inscreva-se</button>
+                <button onClick={() => navigate("/register")} className="w-96 mt-10 h-36 text-5xl font-medium text-white text-center rounded-2xl bg-blue-2 mr-4 max-sm:mr-0  mb-40">Inscreva-se</button>
               </section>
-              <section className=" flex bg-white  ">
-                <div className="w-1/2 flex flex-col items-start p-16 justify-start">
-                    <p className="text-5xl text-blue-1 mb-20">Por que participar
+              <section className=" flex bg-white max-lg:flex-col ">
+                <div className="w-1/2 flex flex-col max-lg:w-full items-start p-16 max-lg:p-2 justify-start">
+                    <p className="text-5xl max-md:text-4xl  text-blue-1 mb-20">Por que participar
                     dos<b> Bootcamps?</b></p>
                        
                     <div className="flex items-center">
-                        <img src="../../../public/icons/stars.svg" alt="estrelas" className="w-20" />
-                        <span className="text-2xl text-blue-1 ml-10">Desenvolva soluções incríveis,<br /> concorrendo a <b>bolsas de estudo.</b></span>
+                        <img src="../../../public/icons/stars.svg" alt="estrelas" className="w-20 max-lg:w-16 max-lg:mr-2" />
+                        <span className="text-4xl text-blue-1 max-lg:ml-0 ml-10">Desenvolva soluções incríveis,<br /> concorrendo a <b>bolsas de estudo.</b></span>
                     </div>
                     
-                    <div className="flex items-center mt-16">
-                        <img src="../../../public/icons/gears.svg" alt="engrenagens" className="w-28" />
-                        <span className="text-2xl text-blue-1 m-3">Ganhe experiência e <br />amplie o seu <br /><b>conhecimento</b></span>
+                    <div className="flex items-center mt-16  ">
+                        <img src="../../../public/icons/gears.svg" alt="engrenagens" className="w-28 max-lg:w-20" />
+                        <span className="text-4xl  text-blue-1 max-lg:mb-10 max-lg:ml-0 m-3">Ganhe experiência e <br />amplie o seu <br /><b>conhecimento</b></span>
                     </div>
                    
                 </div>
                 <div className="m-2 bg-white border border-l-2 border-blue-1"></div>
-                <div className="w-1/2 flex flex-col items-start p-16 justify-start">                    
-                    <p className="text-5xl text-blue-1 mb-20">O quê é o<b> Imersy?</b></p>
+                <div className="w-1/2 flex flex-col items-start p-16 max-lg:p-2 justify-start max-lg:w-full max-lg:h-full max-lg:mb-20">                    
+                    <p className="text-5xl max-md:text-4xl  text-blue-1 mb-20">O quê é o<b> Imersy?</b></p>
                     <span className="text-4xl text-blue-1">uma plataforma unimar, com o objetivo de facilitar a participação dos estudantes em bootcamps</span>
                 </div>
               </section>
--- imersy-frontend/src/pages/home/navbar.tsx
+++ imersy-frontend/src/pages/home/navbar.tsx
@@ -5,11 +5,11 @@ export function NavBar() {
     const navigate = useNavigate()
     return (
         <>
-            <nav className="flex justify-between font-semibold items-center text-white p-2  h-44 bg-blue-3"> 
-              <p className="text-4xl font-plex-mono ml-16 font-semibold text-center">Imersy</p>
-              <div>
-                <button className="w-56 h-16 text-2xl text-center font-semibold rounded-2xl bg-blue-2 mr-4" onClick={() => navigate("/register")}>Inscreva-se</button>
-                <button className="w-40 h-16 text-2xl border-2 font-semibold rounded-2xl border-white mr-9" onClick={() => navigate("/login")}>Entre</button>
+            <nav className="flex justify-between font-semibold items-center text-white p-2 h-44 max-md:20 bg-blue-3"> 
+              <p className="text-4xl font-plex-mono ml-16 max-md:hidden font-semibold text-center max-md:ml-0">Imersy</p>
+              <div className="flex max-md:w-full max-md:h-auto">
+                <button className="w-56 h-16 text-2xl max-md:w-full text-center font-semibold rounded-2xl bg-blue-2 mr-4" onClick={() => navigate("/register")}>Inscreva-se</button>
+                <button className="w-40 h-16 text-2xl max-md:w-full border-2 font-semibold rounded-2xl border-white mr-9 max-md:mr-0" onClick={() => navigate("/login")}>Entre</button>
               </div>
             </nav>
         </>
